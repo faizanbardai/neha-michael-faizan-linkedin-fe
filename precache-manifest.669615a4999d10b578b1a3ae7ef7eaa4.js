1efe9f11fa214bb1c3be966f32eda669b1bc855f
Faizan Bardai <faizan.bardai@celeritas-solutions.com>
2020-01-22 15:52:27 +0100

    Updates

---
+++ precache-manifest.669615a4999d10b578b1a3ae7ef7eaa4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c00494cd9b34041ca282490c003f0e4",
+    "url": "/neha-michael-faizan-linkedin-fe/index.html"
+  },
+  {
+    "revision": "bba2189eef94cdcb45de",
+    "url": "/neha-michael-faizan-linkedin-fe/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b58fa281ce576f6f524d",
+    "url": "/neha-michael-faizan-linkedin-fe/static/js/2.7fd6ff51.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/neha-michael-faizan-linkedin-fe/static/js/2.7fd6ff51.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bba2189eef94cdcb45de",
+    "url": "/neha-michael-faizan-linkedin-fe/static/js/main.26a2adcc.chunk.js"
+  },
+  {
+    "revision": "66224ec8ec15eb7fdf19",
+    "url": "/neha-michael-faizan-linkedin-fe/static/js/runtime-main.f049ae10.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/neha-michael-faizan-linkedin-fe/static/media/logo.5d5d9eef.svg"
+  }
+]);
